af5434610da2eb21af81a0d11615b928826abc05
dani <dani@dani-ThinkPad-T540p>
2016-11-21 08:34:00 +0100

    tonos de colores

--- css/colores.php
+++ css/colores.php
@@ -48,6 +48,13 @@ switch ($estilo_color) {
 		
 		$fondo_menu="#AAAAFF";
 		$udm_flecha="right-navblue.gif";
+		$tono1 = "#ccccff";
+		$tono2 = "#b3b3ff";
+		$tono3 = "#9999ff";
+		$tono4 = "#8080ff";
+		$tono5 = "#6666ff";
+		$tono6 = "#4d4dff";
+		$tono7 = "#3333ff";
 		break;
 	case  "azul":
 		$border=0;
@@ -71,6 +78,14 @@ switch ($estilo_color) {
 		
 		$fondo_menu="#AAAAFF";
 		$udm_flecha="right-navblue.gif";
+		$tono1 = "#ebf0fa";
+		$tono2 = "#d6e1f5";
+		$tono3 = "#c2d3f0";
+		$tono4 = "#adc4eb";
+		$tono5 = "#99b5e6";
+		$tono6 = "#85a6e0";
+		$tono7 = "#7097db";
+
 		break;
 	case "verde":
 		$border=0;
@@ -93,6 +108,14 @@ switch ($estilo_color) {
 		$fondo_tres="#EECCCC";
 		
 		$udm_flecha="right-navgreen.gif";
+		$tono1 = "#f0f5ef";
+		$tono2 = "#e1ecdf";
+		$tono3 = "#d2e2d0";
+		$tono4 = "#c3d8c0";
+		$tono5 = "#b4cfb0";
+		$tono6 = "#a4c5a0";
+		$tono7 = "#95bb90";
+
 		break;
 	case "naranja":
 		$border=0;
@@ -114,6 +137,14 @@ switch ($estilo_color) {
 		
 		$fondo_menu="#FFAAAA";
 		$udm_flecha="right-navorange.gif";
+		$tono1 = "#fff0e6";
+		$tono2 = "#ffe0cc";
+		$tono3 = "#ffd1b3";
+		$tono4 = "#ffc299";
+		$tono5 = "#ffb380";
+		$tono6 = "#ffa366";
+		$tono7 = "#ff944d";
+
 	break;
 }
 ?>
