02d9086a05a403e74041be1121d259e98d48e6cf
dani <dani@dani-ThinkPad-T540p>
2018-02-14 11:16:31 +0100

    poner la fecha de hoy cuando no existe ninguna fecha de acta

--- apps/notas/controller/comprobar_notas.php
+++ apps/notas/controller/comprobar_notas.php
@@ -105,7 +105,7 @@ if ($actualizar=="9999") {
 		//echo "f_actas: $ssql_1<br>";
 		$oDBSt_sql_1=$oDB->query($ssql_1);
 		$f_acta=$oDBSt_sql_1->fetchColumn();
-		if (is_empty($f_acta)) {
+		if (empty($f_acta)) {
 			//pongo la de hoy. creo que actualmente no se utiliza.
 			$f_acta = date('d/m/Y');
 		}
