d4b3549055e4a7d4176960635ebed20120a972ef
dani <dani@dani-ThinkPad-T540p>
2018-03-07 12:59:24 +0100

    No guardar id_tabla. Dejar que sea la base de datos

--- apps/actividades/model/actividadex.class.php
+++ apps/actividades/model/actividadex.class.php
@@ -77,7 +77,7 @@ class ActividadEx Extends ActividadAll {
 		$aDades['tarifa'] = $this->itarifa;
 		$aDades['id_repeticion'] = $this->iid_repeticion;
 		$aDades['publicado'] = $this->bpublicado;
-		$aDades['id_tabla'] = $this->sid_tabla;
+		//$aDades['id_tabla'] = $this->sid_tabla;
 		$aDades['plazas'] = $this->iplazas;
 		array_walk($aDades, 'core\poner_null');
 		//para el caso de los boolean false, el pdo(+postgresql) pone string '' en vez de 0. Lo arreglo:
@@ -107,8 +107,8 @@ class ActividadEx Extends ActividadAll {
 					tarifa                   = :tarifa,
 					id_repeticion            = :id_repeticion,
 					publicado   	         = :publicado,
-					id_tabla   	        	 = :id_tabla,
 					plazas   	        	 = :plazas";
+					//id_tabla   	        	 = :id_tabla,
 			if (($qRs = $oDbl->prepare("UPDATE $nom_tabla SET $update WHERE id_activ='$this->iid_activ'")) === false) {
 				$sClauError = 'ActividadEx.update.prepare';
 				$_SESSION['oGestorErrores']->addErrorAppLastError($oDbl, $sClauError, __LINE__, __FILE__);
@@ -126,8 +126,10 @@ class ActividadEx Extends ActividadAll {
 			}
 		} else {
 			// INSERT
-			$campos="(id_tipo_activ,dl_org,nom_activ,id_ubi,desc_activ,f_ini,h_ini,f_fin,h_fin,tipo_horario,precio,num_asistentes,status,observ,nivel_stgr,observ_material,lugar_esp,tarifa,id_repeticion,publicado,id_tabla,plazas)";
-			$valores="(:id_tipo_activ,:dl_org,:nom_activ,:id_ubi,:desc_activ,:f_ini,:h_ini,:f_fin,:h_fin,:tipo_horario,:precio,:num_asistentes,:status,:observ,:nivel_stgr,:observ_material,:lugar_esp,:tarifa,:id_repeticion,:publicado,:id_tabla,:plazas)";
+			//$campos="(id_tipo_activ,dl_org,nom_activ,id_ubi,desc_activ,f_ini,h_ini,f_fin,h_fin,tipo_horario,precio,num_asistentes,status,observ,nivel_stgr,observ_material,lugar_esp,tarifa,id_repeticion,publicado,id_tabla,plazas)";
+			$campos="(id_tipo_activ,dl_org,nom_activ,id_ubi,desc_activ,f_ini,h_ini,f_fin,h_fin,tipo_horario,precio,num_asistentes,status,observ,nivel_stgr,observ_material,lugar_esp,tarifa,id_repeticion,publicado,plazas)";
+			//$valores="(:id_tipo_activ,:dl_org,:nom_activ,:id_ubi,:desc_activ,:f_ini,:h_ini,:f_fin,:h_fin,:tipo_horario,:precio,:num_asistentes,:status,:observ,:nivel_stgr,:observ_material,:lugar_esp,:tarifa,:id_repeticion,:publicado,:id_tabla,:plazas)";
+			$valores="(:id_tipo_activ,:dl_org,:nom_activ,:id_ubi,:desc_activ,:f_ini,:h_ini,:f_fin,:h_fin,:tipo_horario,:precio,:num_asistentes,:status,:observ,:nivel_stgr,:observ_material,:lugar_esp,:tarifa,:id_repeticion,:publicado,:plazas)";
 			if (($qRs = $oDbl->prepare("INSERT INTO $nom_tabla $campos VALUES $valores")) === false) {
 				$sClauError = 'ActividadEx.insertar.prepare';
 				$_SESSION['oGestorErrores']->addErrorAppLastError($oDbl, $sClauError, __LINE__, __FILE__);
