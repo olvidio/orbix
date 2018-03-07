264b122b5f7eea8dc6bcbb32f52338afc3fbee12
dani <dani@dani-ThinkPad-T540p>
2018-03-07 12:59:11 +0100

    No guardar id_tabla. Dejar que sea la base de datos

--- apps/actividades/model/actividad.class.php
+++ apps/actividades/model/actividad.class.php
@@ -75,7 +75,7 @@ class Actividad Extends ActividadAll {
 		$aDades['tarifa'] = $this->itarifa;
 		$aDades['id_repeticion'] = $this->iid_repeticion;
 		$aDades['publicado'] = $this->bpublicado;
-		$aDades['id_tabla'] = $this->sid_tabla;
+		//$aDades['id_tabla'] = $this->sid_tabla;
 		$aDades['plazas'] = $this->iplazas;
 		array_walk($aDades, 'core\poner_null');
 		//para el caso de los boolean false, el pdo(+postgresql) pone string '' en vez de 0. Lo arreglo:
