ce845efa63fb916f5603de8d4bbebf504c987d48
dani <dani@dani-ThinkCentre-M92p>
2018-03-20 11:40:00 +0100

    ignorar error al intentar conseguir las plazas de un ctr

--- apps/actividadplazas/model/gestorresumenplazas.class.php
+++ apps/actividadplazas/model/gestorresumenplazas.class.php
@@ -223,7 +223,12 @@ class GestorResumenPlazas {
 		if (empty($plazas_totales)) {
 			$id_ubi = $oActividad->getId_ubi();
 			$oCasa = \ubis\model\Ubi::NewUbi($id_ubi);
-			$plazas_totales = $oCasa->getPlazas();
+			// Si la casa es un ctr de otra dl, no sé las plazas
+			if(method_exists($oCasa, 'getPlazas')){
+				$plazas_totales = $oCasa->getPlazas();
+			} else {
+				$plazas_totales = '';
+			}
 			if (empty($plazas_totales)) {
 				$plazas_totales = '?';
 			}
