0cb5cff3ed2bff98c327824c93ebd118cc9fa629
dani <dani@dani-ThinkCentre-M92p>
2018-03-30 12:08:15 +0200

    poner las mismas condiciones de búsqueda que en 'actividad_select.php'

--- apps/asistentes/controller/lista_asis_conjunto_activ.php
+++ apps/asistentes/controller/lista_asis_conjunto_activ.php
@@ -27,25 +27,30 @@ use ubis\model as ubis;
 	require_once ("apps/core/global_object.inc");
 // FIN de  Cabecera global de URL de controlador ********************************
 
-$status = empty($_POST['status'])? 2 : $_POST['status'];
-$id_tipo_activ = empty($_POST['id_tipo_activ'])? '' : $_POST['id_tipo_activ'];
-$id_ubi = empty($_POST['id_ubi'])? '' : $_POST['id_ubi'];
-$periodo = empty($_POST['periodo'])? '' : $_POST['periodo'];
-$inicio = empty($_POST['inicio'])? '' : $_POST['inicio'];
-$fin = empty($_POST['fin'])? '' : $_POST['fin'];
-$year = empty($_POST['year'])? '' : $_POST['year'];
-$dl_org = empty($_POST['dl_org'])? '' : $_POST['dl_org'];
-$empiezamin = empty($_POST['empiezamin'])? date('d/m/Y') : $_POST['empiezamin'];
-$empiezamax = empty($_POST['empiezamax'])? date('d/m/Y',mktime(0, 0, 0, date('m')+6, 0, date('Y'))) : $_POST['empiezamax'];
-
+$Qstatus = empty($_POST['status'])? 2 : $_POST['status'];
+$Qid_tipo_activ = empty($_POST['id_tipo_activ'])? '' : $_POST['id_tipo_activ'];
+$Qid_ubi = empty($_POST['id_ubi'])? '' : $_POST['id_ubi'];
+$Qperiodo = empty($_POST['periodo'])? '' : $_POST['periodo'];
+$Qinicio = empty($_POST['inicio'])? '' : $_POST['inicio'];
+$Qfin = empty($_POST['fin'])? '' : $_POST['fin'];
+$Qyear = empty($_POST['year'])? (integer) date('Y') : $_POST['year'];
+$Qdl_org = empty($_POST['dl_org'])? '' : $_POST['dl_org'];
+$Qempiezamin = empty($_POST['empiezamin'])? date('d/m/Y',mktime(0, 0, 0, date('m'), date('d')-40, date('Y'))) : $_POST['empiezamin'];
+$Qempiezamax = empty($_POST['empiezamax'])? date('d/m/Y',mktime(0, 0, 0, date('m')+9, 0, date('Y'))) : $_POST['empiezamax'];
+	
 // Condiciones de búsqueda.
+$aWhere = array();
 // Status
-if ($status!=5) {
-	$aWhere['status'] = $status;
+if ($Qstatus!=5) {
+	$aWhere['status'] = $Qstatus;
 }
 // Id tipo actividad
-if (empty($id_tipo_activ)) {
-	$ssfsv = empty($_POST['ssfsv'])? '.' : $_POST['ssfsv'];
+if (empty($Qid_tipo_activ)) {
+	if (empty($_POST['ssfsv'])) {
+		if ($mi_sfsv == 1) $_POST['ssfsv'] = 'sv';
+		if ($mi_sfsv == 2) $_POST['ssfsv'] = 'sf';
+	}
+	$ssfsv = $_POST['ssfsv'];
 	$sasistentes = empty($_POST['sasistentes'])? '.' : $_POST['sasistentes'];
 	$sactividad = empty($_POST['sactividad'])? '.' : $_POST['sactividad'];
 	$snom_tipo = empty($_POST['snom_tipo'])? '...' : $_POST['snom_tipo'];
@@ -53,42 +58,51 @@ if (empty($id_tipo_activ)) {
 	$oTipoActiv->setSfsvText($ssfsv);
 	$oTipoActiv->setAsistentesText($sasistentes);
 	$oTipoActiv->setActividadText($sactividad);
-	$id_tipo_activ=$oTipoActiv->getId_tipo_activ();
+	$Qid_tipo_activ=$oTipoActiv->getId_tipo_activ();
 } else {
-	$oTipoActiv= new web\TiposActividades($id_tipo_activ);
+	$oTipoActiv= new web\TiposActividades($Qid_tipo_activ);
 	$ssfsv=$oTipoActiv->getSfsvText();
 	$sasistentes=$oTipoActiv->getAsistentesText();
 	$sactividad=$oTipoActiv->getActividadText();
 	$nom_tipo=$oTipoActiv->getNom_tipoText();
 }
-if ($id_tipo_activ!='......') {
-	$aWhere['id_tipo_activ'] = "^$id_tipo_activ";
+if ($Qid_tipo_activ!='......') {
+	$aWhere['id_tipo_activ'] = "^$Qid_tipo_activ";
 	$aOperador['id_tipo_activ'] = '~';
 } 
 // Lugar
-if (!empty($id_ubi)) {
-	$aWhere['id_ubi']=$id_ubi;
+if (!empty($Qid_ubi)) {
+	$aWhere['id_ubi']=$Qid_ubi;
 }
 // periodo.
-if (empty($periodo) || $periodo == 'otro') {
-	$inicio = empty($inicio)? $empiezamin : $inicio;
-	$fin = empty($fin)? $empiezamax : $fin;
+if (empty($Qperiodo) || $Qperiodo == 'otro') {
+	$Qinicio = empty($Qinicio)? $Qempiezamin : $Qinicio;
+	$Qfin = empty($Qfin)? $Qempiezamax : $Qfin;
 } else {
 	$oPeriodo = new web\Periodo();
-	$any=empty($year)? date('Y')+1 : $year;
+	$any=empty($Qyear)? date('Y')+1 : $Qyear;
 	$oPeriodo->setAny($any);
-	$oPeriodo->setPeriodo($periodo);
-	$inicio = $oPeriodo->getF_ini();
-	$fin = $oPeriodo->getF_fin();
+	$oPeriodo->setPeriodo($Qperiodo);
+	$Qinicio = $oPeriodo->getF_ini();
+	$Qfin = $oPeriodo->getF_fin();
 }
-$aWhere['f_ini'] = "'$inicio','$fin'";
-$aOperador['f_ini'] = 'BETWEEN';
+if (!empty($Qperiodo) && $Qperiodo == 'desdeHoy') {
+	$aWhere['f_fin'] = "'$Qinicio','$Qfin'";
+	$aOperador['f_fin'] = 'BETWEEN';
+} else {
+	$aWhere['f_ini'] = "'$Qinicio','$Qfin'";
+	$aOperador['f_ini'] = 'BETWEEN';
+}
 // dl Organizadora.
-if (!empty($_POST['dl_org'])) {
-   $aWhere['dl_org'] = $_POST['dl_org']; 
+if (!empty($Qdl_org)) {
+   $aWhere['dl_org'] = $Qdl_org; 
 }
+// Publicar
+if (!empty($Qmodo) && $Qmodo == 'publicar') {
+   $aWhere['publicado'] = 'f'; 
+}
+
 $aWhere['_ordre'] = 'f_ini';
-//echo"query:$query <br>";
 $GesActividades = new actividades\GestorActividad();
 $cActividades = $GesActividades->getActividades($aWhere,$aOperador);
 
