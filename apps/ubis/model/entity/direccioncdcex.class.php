da12063ffe1115ff50afcd96c3f35fb8821869e8
dani <dani@dani-ThinkPad-T540p>
2018-05-25 12:12:56 +0200

    error con id_auto

--- apps/ubis/model/entity/direccioncdcex.class.php
+++ apps/ubis/model/entity/direccioncdcex.class.php
@@ -189,7 +189,6 @@ class DireccionCdcEx Extends DireccionCdc {
 	 */
 	function setAllAtributes($aDades) {
 		if (!is_array($aDades)) return;
-		if (array_key_exists('id_auto',$aDades)) $this->setId_auto($aDades['id_auto']);
 		if (array_key_exists('id_direccion',$aDades)) $this->setId_direccion($aDades['id_direccion']);
 		if (array_key_exists('direccion',$aDades)) $this->setDireccion($aDades['direccion']);
 		if (array_key_exists('c_p',$aDades)) $this->setC_p($aDades['c_p']);
