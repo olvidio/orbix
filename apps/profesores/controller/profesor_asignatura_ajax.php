910b51af7367ad58175daa3588055eea8af013f7
dani <dani@dani-ThinkPad-T540p>
2019-03-28 09:51:35 +0100

    Añadir Docencia, Errores #125

--- apps/profesores/controller/profesor_asignatura_ajax.php
+++ apps/profesores/controller/profesor_asignatura_ajax.php
@@ -1,6 +1,7 @@
 <?php
 use profesores\model\entity as profesores;
 use personas\model\entity as personas;
+use profesores\model\entity\GestorProfesorDocenciaStgr;
 
 // INICIO Cabecera global de URL de controlador *********************************
 	require_once ("apps/core/global_header.inc");
@@ -24,8 +25,10 @@ $cProfesores = $GesProfesores->getListaProfesoresAsignatura($Qid_asignatura);
 //$a_botones=array( array( 'txt' => _("modificar"), 'click' =>"fnjs_modificar(this.form)" ) );
 $a_botones=array();
 
+$a_cabeceras = [];
 $a_cabeceras[]= array('name'=>ucfirst(_("apellidos, nombre")),'formatter'=>'clickFormatter');
 $a_cabeceras[]= ucfirst(_("centro"));
+$a_cabeceras[]= ucfirst(_("docencia"));
 $a_cabeceras[]= ucfirst(_("teléfono"));
 $a_cabeceras[]= ucfirst(_("mail"));
 	  
@@ -35,6 +38,18 @@ foreach ($cProfesores['departamento'] as $id_nom => $ap_nom) {
 	$i++;
 	$oPersonaDl = new personas\PersonaDl($id_nom);
 	$centro = $oPersonaDl->getCentro_o_dl();
+	// Actividad docente
+    $oGesDocencia = new GestorProfesorDocenciaStgr();
+    $cDocencia = $oGesDocencia->getProfesorDocenciasStgr(['id_nom' => $id_nom, 'id_asignatura' => $Qid_asignatura]);
+    $txt_docencia = '';
+    foreach ($cDocencia as $oProfesorDacendiaStgr) {
+        $inicio_curso = $oProfesorDacendiaStgr->getCurso_inicio();
+        $fin_curso = $inicio_curso + 1;
+        $curso = "$inicio_curso-$fin_curso";
+        $txt_docencia .= !empty($txt_docencia)? '; ' : '';
+        $txt_docencia .= $curso;
+    }
+	// Telecos
 	$gesTelecoPersona = new personas\GestorTelecoPersonaDl();
 	$cTelecoPersona = $gesTelecoPersona->getTelecos(array('id_nom'=>$id_nom));
 	$telfs = '';	
@@ -58,14 +73,27 @@ foreach ($cProfesores['departamento'] as $id_nom => $ap_nom) {
 	$a_valores[$i]['sel']="$id_nom";
 	$a_valores[$i][1]= array( 'ira'=>$pagina, 'valor'=>$ap_nom);
 	$a_valores[$i][2]=$centro;
-	$a_valores[$i][3]=$telfs;
-	$a_valores[$i][4]=$mails;
+	$a_valores[$i][3]=$txt_docencia;
+	$a_valores[$i][4]=$telfs;
+	$a_valores[$i][5]=$mails;
 }
 // Para añadir los de apmpliación
 foreach ($cProfesores['ampliacion'] as $id_nom => $ap_nom) {
 	$i++;
 	$oPersonaDl = new personas\PersonaDl($id_nom);
 	$centro = $oPersonaDl->getCentro_o_dl();
+	// Actividad docente
+    $oGesDocencia = new GestorProfesorDocenciaStgr();
+    $cDocencia = $oGesDocencia->getProfesorDocenciasStgr(['id_nom' => $id_nom, 'id_asignatura' => $Qid_asignatura]);
+    $txt_docencia = '';
+    foreach ($cDocencia as $oProfesorDacendiaStgr) {
+        $inicio_curso = $oProfesorDacendiaStgr->getCurso_inicio();
+        $fin_curso = $inicio_curso + 1;
+        $curso = "$inicio_curso-$fin_curso";
+        $txt_docencia .= !empty($txt_docencia)? '; ' : '';
+        $txt_docencia .= $curso;
+    }
+	// Telecos
 	$gesTelecoPersona = new personas\GestorTelecoPersonaDl();
 	$cTelecoPersona = $gesTelecoPersona->getTelecos(array('id_nom'=>$id_nom));
 	$telfs = '';	
@@ -89,8 +117,9 @@ foreach ($cProfesores['ampliacion'] as $id_nom => $ap_nom) {
 	$a_valores[$i]['sel']="$id_nom";
 	$a_valores[$i][1]= array( 'ira'=>$pagina, 'valor'=>$ap_nom);
 	$a_valores[$i][2]=$centro;
-	$a_valores[$i][3]=$telfs;
-	$a_valores[$i][4]=$mails;
+	$a_valores[$i][3]=$txt_docencia;
+	$a_valores[$i][4]=$telfs;
+	$a_valores[$i][5]=$mails;
 
 }
 
