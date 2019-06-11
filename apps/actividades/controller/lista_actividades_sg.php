47a4e2878045ba100a56f734017cf5a1be463881
dani <dani@dani-ThinkPad-T540p>
2019-06-11 17:10:01 +0200

    más menus sg

---
+++ apps/actividades/controller/lista_actividades_sg.php
@@ -0,0 +1,431 @@
+<?php
+/**
+* Esta página muestra una tabla con las actividades que cumplen con la condicion.
+* He quitado la posibilidad de buscar por sacd i por ctr. Quedan las opciones:
+*
+*@param 	$que
+*        	$status por defecto = 2
+*        	$id_tipo_activ
+*        	$id_ubi
+*        	$periodo 
+*        	$inicio
+*        	$fin 
+*        	$year
+*        	$dl_org 
+*        	$empiezamin por defecto = 15-oct
+* 		 	$empiezamax por defecto = 15-jun
+*
+* Si el resultado es más de 200, pregunta si quieres seguir.
+*
+*@package	delegacion
+*@subpackage	actividades
+*@author	Daniel Serrabou
+*@since		15/5/02.
+*@ajax		23/8/2007.		
+*@last		23/4/2012.		
+*		
+*/
+
+// INICIO Cabecera global de URL de controlador *********************************
+use actividadcargos\model\entity\GestorActividadCargo;
+use actividades\model\entity\ActividadAll;
+use actividades\model\entity\GestorActividad;
+use actividadescentro\model\entity\GestorCentroEncargado;
+use core\ConfigGlobal;
+use function core\strtoupper_dlb;
+use permisos\model\PermisosActividadesTrue;
+use ubis\model\entity\Ubi;
+use usuarios\model\entity\Preferencia;
+use web\DateTimeLocal;
+use web\Lista;
+use web\Periodo;
+use web\PeriodoQue;
+use web\TiposActividades;
+
+require_once ("apps/core/global_header.inc");
+// Arxivos requeridos por esta url **********************************************
+
+// Crea los objectos de uso global **********************************************
+require_once ("apps/core/global_object.inc");
+// FIN de  Cabecera global de URL de controlador ********************************
+
+// Declarción de variables ******************************************************
+$num_max_actividades = 200;
+
+$mi_sfsv = core\ConfigGlobal::mi_sfsv();
+
+$oPosicion->recordar();
+
+$Qcontinuar = (string)  filter_input(INPUT_POST, 'continuar');
+// Sólo sirve para esta pagina: importar, publicar, duplicar
+$QGstack = (integer)  filter_input(INPUT_POST, 'Gstack');
+if (isset($_POST['stack'])) {
+    $stack = \filter_input(INPUT_POST, 'stack', FILTER_SANITIZE_NUMBER_INT);
+} else {
+    $stack = '';
+}
+
+//Si vengo de vuelta con el parámetro 'continuar', los datos no están en el POST,
+// sino en $Posicion. Le paso la referecia del stack donde está la información.
+if (!empty($Qcontinuar) && $Qcontinuar == 'si' && ($QGstack != '')) {
+    $oPosicion->goStack($QGstack);
+    $Qque = $oPosicion->getParametro('que');
+    $Qstatus = $oPosicion->getParametro('status');
+    $Qid_tipo_activ = $oPosicion->getParametro('id_tipo_activ');
+    $Qfiltro_lugar = $oPosicion->getParametro('filtro_lugar');
+    $Qid_ubi= $oPosicion->getParametro('id_ubi');
+    $Qperiodo=$oPosicion->getParametro('periodo');
+    $Qinicio=$oPosicion->getParametro('inicio');
+    $Qfin=$oPosicion->getParametro('fin');
+    $Qyear=$oPosicion->getParametro('year');
+    $Qdl_org=$oPosicion->getParametro('dl_org');
+    $Qempiezamin=$oPosicion->getParametro('empiezamin');
+    $Qempiezamax=$oPosicion->getParametro('empiezamax');
+    $Qid_sel=$oPosicion->getParametro('id_sel');
+    $Qscroll_id = $oPosicion->getParametro('scroll_id');
+    $oPosicion->olvidar($QGstack); //limpio todos los estados hacia delante.
+} else { //si vengo de vuelta y tengo los parametros en el $_POST
+    $Qid_sel = (array)  \filter_input(INPUT_POST, 'sel', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+    $Qscroll_id = (string) \filter_input(INPUT_POST, 'scroll_id');
+    //Si vengo por medio de Posicion, borro la última
+    if ($stack != '') {
+        // No me sirve el de global_object, sino el de la session
+        $oPosicion2 = new web\Posicion();
+        if ($oPosicion2->goStack($stack)) { // devuelve false si no puede ir
+            $Qid_sel=$oPosicion2->getParametro('id_sel');
+            $Qscroll_id = $oPosicion2->getParametro('scroll_id');
+            $oPosicion2->olvidar($stack);
+        }
+    }
+    $Qque = (string) \filter_input(INPUT_POST, 'que');
+    $Qstatus = (integer) \filter_input(INPUT_POST, 'status');
+    $Qtipo_activ_sg = (string) \filter_input(INPUT_POST, 'tipo_activ_sg');
+    $Qfiltro_lugar = (string) \filter_input(INPUT_POST, 'filtro_lugar');
+    $Qid_ubi = (integer) \filter_input(INPUT_POST, 'id_ubi');
+    $Qperiodo = (string) \filter_input(INPUT_POST, 'periodo');
+    $Qinicio = (string) \filter_input(INPUT_POST, 'inicio');
+    $Qfin = (string) \filter_input(INPUT_POST, 'fin');
+    $Qyear = (string) \filter_input(INPUT_POST, 'year');
+    $Qdl_org = (string) \filter_input(INPUT_POST, 'dl_org');
+    
+    $Qempiezamin = (string) \filter_input(INPUT_POST, 'empiezamin');
+    $Qempiezamax = (string) \filter_input(INPUT_POST, 'empiezamax');
+    
+    if (empty($Qempiezamin)) {
+        switch ($Qtipo_activ_sg) {
+            case 'crt':
+                $any = $_SESSION['oConfig']->any_final_curs('crt');
+                $QempiezaminIso = core\curso_est("inicio",$any,"crt")->format('Y-m-d');
+                break;
+            case 'cv':
+                $any = $_SESSION['oConfig']->any_final_curs('est');
+                $QempiezaminIso = core\curso_est("inicio",$any,"est")->format('Y-m-d');
+                break;
+        }
+    } else {
+        $oEmpiezamin = DateTimeLocal::createFromLocal($Qempiezamin);
+        $QempiezaminIso = $oEmpiezamin->getIso();
+    }
+    if (empty($Qempiezamax)) {
+        switch ($Qtipo_activ_sg) {
+            case 'crt':
+                $any = $_SESSION['oConfig']->any_final_curs('crt');
+                $QempiezamaxIso = core\curso_est("fin",$any,"crt")->format('Y-m-d');
+                break;
+            case 'cv':
+                $any = $_SESSION['oConfig']->any_final_curs('est');
+                $QempiezamaxIso = core\curso_est("fin",$any,"est")->format('Y-m-d');
+                break;
+        }
+    } else {
+        $oEmpiezamax = DateTimeLocal::createFromLocal($Qempiezamax);
+        $QempiezamaxIso = $oEmpiezamax->getIso();
+    }
+	
+	$Qstatus = empty($Qstatus)? ActividadAll::STATUS_ACTUAL : $Qstatus;
+	
+	$aGoBack = array (
+	    'que'=>$Qque,
+	    'tipo_activ_sg'=>$Qtipo_activ_sg,
+	    'id_ubi'=>$Qid_ubi,
+	    'periodo'=>$Qperiodo,
+	    'year'=>$Qyear,
+	    'inicio'=>$Qinicio,
+	    'fin'=>$Qfin,
+	    'dl_org'=>$Qdl_org,
+	    'status'=>$Qstatus,
+	    'empiezamin'=>$Qempiezamin,
+	    'empiezamax'=>$Qempiezamax );
+	$oPosicion->setParametros($aGoBack,1);
+}
+
+// Condiciones de búsqueda.
+$aWhere = [];
+$aOperador = [];
+// Status
+if ($Qstatus!=5) {
+	$aWhere['status'] = $Qstatus;
+}
+// Id tipo actividad
+if (empty($Qtipo_activ_sg)) {
+	$Qtipo_activ_sg='crt';
+}
+switch ($Qtipo_activ_sg) {
+	case 'crt':
+		$Qid_tipo_activ='1[45]1';
+		break;
+	case 'cv':
+		$Qid_tipo_activ='1[45]3';
+		break;
+}
+$aWhere['id_tipo_activ'] = "^$Qid_tipo_activ";
+$aOperador['id_tipo_activ'] = '~';
+
+// Lugar
+if (!empty($Qid_ubi)) {
+	$aWhere['id_ubi']=$Qid_ubi;
+}
+// periodo.
+if (empty($Qperiodo) || $Qperiodo == 'otro') {
+	$Qinicio = empty($Qinicio)? $QempiezaminIso : $Qinicio;
+	$Qfin = empty($Qfin)? $QempiezamaxIso : $Qfin;
+} else {
+	$oPeriodo = new Periodo();
+	$any=empty($Qyear)? date('Y')+1 : $Qyear;
+	$oPeriodo->setAny($any);
+	$oPeriodo->setPeriodo($Qperiodo);
+	$Qinicio = $oPeriodo->getF_ini_iso();
+	$Qfin = $oPeriodo->getF_fin_iso();
+}
+if (!empty($Qperiodo) && $Qperiodo == 'desdeHoy') {
+	$aWhere['f_fin'] = "'$Qinicio','$Qfin'";
+	$aOperador['f_fin'] = 'BETWEEN';
+} else {
+	$aWhere['f_ini'] = "'$Qinicio','$Qfin'";
+	$aOperador['f_ini'] = 'BETWEEN';
+}
+// dl Organizadora.
+if (!empty($Qdl_org)) {
+   $aWhere['dl_org'] = $Qdl_org; 
+}
+		
+$GesActividades=new GestorActividad();
+
+$a_botones=array( 
+			array( 'txt' => _('cargos'), 'click' =>"jsForm.mandar(\"#seleccionados\",\"carg\")") ,
+			array( 'txt' => _('asistentes'), 'click' =>"jsForm.mandar(\"#seleccionados\",\"asis\")"),
+			array( 'txt' => _('lista'), 'click' =>"jsForm.mandar(\"#seleccionados\",\"list\")"),
+			array( 'txt' => _('ctrs org'), 'click' =>"jsForm.mandar(\"#seleccionados\",\"ctrs\")"),
+		);
+
+$a_cabeceras= array();
+$a_cabeceras[]= array('name'=>_("inicio"),'width'=>40,'class'=>'fecha');
+$a_cabeceras[]=	array('name'=>_("fin"),'width'=>40,'class'=>'fecha');
+$a_cabeceras[]= array('name'=>_("sf"),'width'=>40);
+$a_cabeceras[]= array('name'=>ucfirst(_("tipo")),'width'=>30);
+$a_cabeceras[]= array('name'=>ucfirst(_("asist.")),'width'=>30);
+$a_cabeceras[]= ucfirst(_("lugar"));
+$a_cabeceras[]= ucfirst(_("ctrs"));
+$a_cabeceras[]= ucfirst(_("sacd"));
+$a_cabeceras[]= ucfirst(_("precio"));
+
+$aWhere['_ordre'] = 'f_ini';
+$cActividades = $GesActividades->getActividades($aWhere,$aOperador);
+$num_activ=count($cActividades);
+if ($num_activ > $num_max_actividades && empty($Qcontinuar)) {
+    $go_avant=web\Hash::link(core\ConfigGlobal::getWeb().'/apps/actividades/controller/actividad_select.php?'.http_build_query(array('continuar'=>'si','stack'=>$oPosicion->getStack())));
+    $go_atras=web\Hash::link(core\ConfigGlobal::getWeb().'/apps/actividades/controller/actividad_que.php?'.http_build_query(array('stack'=>$oPosicion->getStack())));
+    echo "<h2>".sprintf(_("son %s actividades a mostrar. ¿Seguro que quiere continuar?."),$num_activ).'</h2>';
+    echo "<input type='button' onclick=fnjs_update_div('#main','".$go_avant."') value="._("continuar").">";
+    echo "<input type='button' onclick=fnjs_update_div('#main','".$go_atras."') value="._("volver").">";
+    die();
+}
+
+$i=0;
+$sin=0;
+$a_valores=array();
+$sPrefs = '';
+$id_usuario= ConfigGlobal::mi_id_usuario();
+$tipo = 'tabla_presentacion';
+$oPref = new Preferencia(array('id_usuario'=>$id_usuario,'tipo'=>$tipo));
+$sPrefs=$oPref->getPreferencia();
+foreach($cActividades as $oActividad) {
+	$id_activ = $oActividad->getId_activ();
+	$id_tipo_activ = $oActividad->getId_tipo_activ();
+	$nom_activ = $oActividad->getNom_activ();
+	$dl_org = $oActividad->getDl_org();
+	$id_ubi = $oActividad->getId_ubi();
+	$lugar_esp = $oActividad->getLugar_esp();
+	$f_ini = $oActividad->getF_ini()->getFromLocal();
+	$f_fin = $oActividad->getF_fin()->getFromLocal();
+	$h_ini = $oActividad->getH_ini();
+	$h_fin = $oActividad->getH_fin();
+	$precio = $oActividad->getPrecio();
+	$observ = $oActividad->getObserv();
+
+	// mirar permisos.
+	if(ConfigGlobal::is_app_installed('procesos')) {
+        $_SESSION['oPermActividades']->setActividad($id_activ,$id_tipo_activ,$dl_org);
+        $oPermActiv = $_SESSION['oPermActividades']->getPermisoActual('datos');
+        $oPermSacd = $_SESSION['oPermActividades']->getPermisoActual('sacd');
+	} else {
+	    $oPermActividades = new PermisosActividadesTrue(ConfigGlobal::mi_id_usuario());
+	    $oPermActiv = $oPermActividades->getPermisoActual('datos');
+	    $oPermSacd =  $oPermActividades->getPermisoActual('sacd');
+	}
+	$i++;
+
+	$oTipoActividad= new TiposActividades($id_tipo_activ);
+	$isfsv=$oTipoActividad->getSfsvId();
+	// para ver el nombre en caso de la otra sección
+	if ($mi_sfsv != $isfsv && !($_SESSION['oPerm']->have_perm("des")) ) {
+	    $ssfsv=$oTipoActividad->getSfsvText();
+	    $sactividad=$oTipoActividad->getActividadText();
+	    $nom_activ="$ssfsv $sactividad";
+	}
+	
+	$ssfsv=$oTipoActividad->getSfsvText();
+	$sasistentes=$oTipoActividad->getAsistentesText();
+	$sactividad=$oTipoActividad->getActividadText();
+	$nom_tipo=$oTipoActividad->getNom_tipoText();
+	if (core\ConfigGlobal::is_app_installed('procesos') && $oPermActiv->have_perm_activ('ocupado') === false) { $sin++; continue; } // no tiene permisos ni para ver.
+	if (core\ConfigGlobal::is_app_installed('procesos') && $oPermActiv->have_perm_activ('ver') === false) { // sólo puede ver que està ocupado
+		$a_valores[$i]['sel']='';
+		$a_valores[$i][1]=sprintf(_( 'ocupado %s (%s-%s)'),$ssfsv,$f_ini,$f_fin);
+		//$a_valores[$i][1]= array( 'ira'=>'x', 'valor'=>'ocupado');
+		$a_valores[$i][2]='';
+		$a_valores[$i][3]='';
+		$a_valores[$i][4]='';
+		$a_valores[$i][5]='';
+		$a_valores[$i][6]='';
+		$a_valores[$i][7]='';
+		$a_valores[$i][8]='';
+		$a_valores[$i][9]='';
+
+	} else {
+		// ubi
+		if (!empty($id_ubi) && $id_ubi != 1) {
+			$oCasa = new Ubi($id_ubi);
+			$nombre_ubi=$oCasa->getNombre_ubi();
+		} else {
+			if ($id_ubi==1 && $lugar_esp) $nombre_ubi=$lugar_esp;
+			if (!$id_ubi && !$lugar_esp) $nombre_ubi=_("sin determinar");
+		}
+
+		// sacd	
+		$oCargosActividad=new GestorActividadCargo();
+		$sacds="";
+		if ($oPermSacd->have_perm('ver') === true) { // sólo si tiene permiso
+			foreach($oCargosActividad->getActividadSacds($id_activ) as $oPersona) {;
+				$sacds.=$oPersona->getApellidosNombre()."# "; // la coma la utilizo como separador de apellidos, nombre.
+			}
+			$sacds=substr($sacds,0,-2);
+		}
+		//ctrs encargados.
+		$oEnc=new GestorCentroEncargado();
+		$ctrs="";
+		foreach($oEnc->getCentrosEncargadosActividad($id_activ) as $oEncargado) {
+			$ctrs.=$oEncargado->getNombre_ubi().", ";
+		}
+		$ctrs=substr($ctrs,0,-2);
+		
+		//coincidente con sf.
+		$coincide = $GesActividades->getCoincidencia($oActividad,'bool');
+		$con = ($coincide)? '*' : '';
+	
+		$a_valores[$i]['sel']="$id_activ#$nom_activ";
+		$a_valores[$i][1]=$f_ini;
+		$a_valores[$i][2]=$f_fin;
+		$a_valores[$i][3]=$con;
+		$a_valores[$i][4]=$sactividad; //ctr
+		$a_valores[$i][5]=$sasistentes; //asist
+		$a_valores[$i][6]=$nombre_ubi; //asist
+		$a_valores[$i][7]=$ctrs;
+		$a_valores[$i][8]=$sacds;
+		$a_valores[$i][9]=$precio;
+	}
+}
+$num=$i;
+if (!empty($a_valores)) {
+    if (isset($Qid_sel) && !empty($Qid_sel)) { $a_valores['select'] = $Qid_sel; }
+    if (isset($Qscroll_id) && !empty($Qscroll_id)) { $a_valores['scroll_id'] = $Qscroll_id; }
+}
+
+$aOpciones =  array(
+                'tot_any' => _('todo el año'),
+                'trimestre_1'=>_('primer trimestre'),
+                'trimestre_2'=>_('segundo trimestre'),
+                'trimestre_3'=>_('tercer trimestre'),
+                'trimestre_4'=>_('cuarto trimestre'),
+                'separador'=>'---------',
+                'curso_ca'=>_('curso ca'),
+                'curso_crt'=>_('curso crt'),
+                'separador1'=>'---------',
+                'otro'=>_('otro')
+                );
+
+$oFormP = new PeriodoQue();
+$oFormP->setFormName('modifica');
+$oFormP->setAntes(strtoupper_dlb(_("periodo")));
+$oFormP->setPosiblesPeriodos($aOpciones);
+$oFormP->setDesplPeriodosOpcion_sel($Qperiodo);
+$oFormP->setDesplAnysOpcion_sel($Qyear);
+$oFormP->setEmpiezaMin($Qempiezamin);
+$oFormP->setEmpiezaMax($Qempiezamax);
+
+$oHash = new web\Hash();
+$oHash->setUrl('apps/actividades/controller/lista_actividades_sg.php');
+$a_camposHidden = array(
+    'que' => $Qque,
+    'tipo_activ_sg' => $Qtipo_activ_sg,
+    'id_ubi' => $Qid_ubi,
+    'periodo' => $Qperiodo,
+    'year' => $Qyear,
+    'dl_org' => $Qdl_org,
+    'status' => $Qstatus,
+    'empiezamin' => $Qempiezamin,
+    'empiezamax' => $Qempiezamax,
+    'filtro_lugar' => $Qfiltro_lugar,
+);
+$oHash->setArraycamposHidden($a_camposHidden);
+$oHash->setCamposNo('modo!id_tipo_activ!id_ubi!periodo!year!dl_org!status!empiezamin!empiezamax!filtro_lugar');
+
+$oHashSel = new web\Hash();
+$oHashSel->setcamposForm('!sel!mod!queSel');
+$oHashSel->setcamposNo('continuar!scroll_id');
+$a_camposHiddenSel = array(
+    'pau' =>'a',
+    'permiso' =>'3',
+    'tabla' => 'a_actividades',
+    'tabla_pau' => 'a_actividades',
+    'Gstack' => $oPosicion->getStack(),
+);
+$oHashSel->setArraycamposHidden($a_camposHiddenSel);
+	
+$oTabla = new web\Lista();
+$oTabla->setId_tabla('actividad_select');
+$oTabla->setCabeceras($a_cabeceras);
+$oTabla->setBotones($a_botones);
+$oTabla->setDatos($a_valores);
+
+$oTabla = new Lista();
+$oTabla->setId_tabla('lista_actividades_sg');
+$oTabla->setCabeceras($a_cabeceras);
+$oTabla->setBotones($a_botones);
+$oTabla->setDatos($a_valores);
+
+$result_busqueda = sprintf( _("%s actividades encontradas (%s sin permiso)"),$num,$sin);
+
+$a_campos = ['oPosicion' => $oPosicion,
+    'oFormP' => $oFormP,
+    'oHash' => $oHash,
+    'oHashSel' => $oHashSel,
+    'Qid_tipo_activ' => $Qid_tipo_activ,
+    'que' => $Qque,
+    'oTabla' => $oTabla,
+    'result_busqueda' => $result_busqueda,
+];
+
+
+$oView = new core\View('actividades/controller');
+echo $oView->render('lista_actividades_sg.phtml',$a_campos);
---
+++ apps/ubis/controller/lista_ctrs.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* Lista con los datos básicos de los cp.
+*
+*@package	delegacion
+*@subpackage	sg
+*@author	Daniel Serrabou
+*@since		6/10/08.
+*		
+*/
+// INICIO Cabecera global de URL de controlador *********************************
+use personas\model\entity\GestorPersonaS;
+use ubis\model\entity\GestorCentroDl;
+use web\Lista;
+
+require_once ("apps/core/global_header.inc");
+// Arxivos requeridos por esta url **********************************************
+
+// Crea los objectos de uso global **********************************************
+require_once ("apps/core/global_object.inc");
+// FIN de  Cabecera global de URL de controlador ********************************
+
+//Para los centros de la dlb
+$GesCentros = new GestorCentroDl();
+$cCentros = $GesCentros->getCentros(array('tipo_ctr'=>'(sm|sj)(ce)*','status'=>'t','_ordre'=>'nombre_ubi'),array('tipo_ctr'=>'~'));
+
+$GesPersonas = new GestorPersonaS();
+$num_total_s = 0;
+$a_valores = array();
+$i=0;
+foreach ($cCentros as $oCentro) {
+	$i++;
+	$id_ubi = $oCentro->getId_ubi();
+	$nombre_ubi = $oCentro->getNombre_ubi();
+	$cPersonasCtr = $GesPersonas->getPersonasDl(array('id_ctr'=>$id_ubi,'situacion'=>'A'));
+	if ($cPersonasCtr  === false) exit(_('error'));
+	$num_s = count($cPersonasCtr);
+	$num_total_s += $num_s;
+
+	
+	$a_valores[$i][1] = $nombre_ubi;
+	$a_valores[$i][2] = $num_s;
+
+}
+	
+$a_cabeceras=array( ucfirst(_('centro')),
+					_('num s')
+				);
+
+$oTabla = new Lista();
+$oTabla->setId_tabla('lista_ctrs');
+$oTabla->setCabeceras($a_cabeceras);
+$oTabla->setDatos($a_valores);
+
+echo "<h3>".ucfirst(sprintf(_("número total de s: %s"),$num_total_s))."</h3>";
+echo $oTabla->mostrar_tabla();
