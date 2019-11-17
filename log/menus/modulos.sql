3338807544c5aa271f46525a7636c607a40cdfbe
dani <dani@dani-ThinkPad-T540p>
2019-11-17 12:29:44 +0100

    modulos sql

---
+++ log/menus/modulos.sql
@@ -0,0 +1,16 @@
+2	devel		{1}	{7}
+3	ubis		{1}	{8}
+4	stgr1		{1}	{9,10,11,12}
+7	stgr2		{4}	{16}
+8	plazas		{5}	{17}
+6	activ2		{5}	{15}
+9	sincro_listas		{1}	{18}
+11	procesos	\N	{5}	{19}
+20	EncargadosActiv	sacd y ctr encargados de actividades	{1,3,6}	{20,21}
+24	zonassacd	distribuión de sacd por zonas	{1}	{8,9,23}
+25	cartaspresentacion	\N	{1,3}	{24}
+21	encargossacd	encargossacd	{1,3}	{9,22}
+1	base	\N	{}	{1,2,3,4,5,6,27}
+26	avisos	\N	{20,21,1}	{26}
+5	activ1	\N	{1}	{9,13,14,28}
+27	economia	ingresos y gastos de las casas	{3,5}	{29}
