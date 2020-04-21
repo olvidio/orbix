29ab73764ed88dfe2b841a11a75f85dcf9f730d6
dani <dani@dani-ThinkPad-T540p>
2020-04-21 09:33:15 +0200

    comunicacion sacd. seleccion activ

--- apps/actividadessacd/controller/com_sacd_activ.php
+++ apps/actividadessacd/controller/com_sacd_activ.php
@@ -1,14 +1,12 @@
 <?php
-use actividadcargos\model\GestorCargoOAsistente;
+use actividadcargos\model\entity\GestorActividadCargo;
 use actividadcargos\model\entity\GestorCargo;
-use actividades\model\entity\GestorActividad;
 use actividadescentro\model\entity\GestorCentroEncargado;
 use actividadessacd\model\ActividadesSacdFunciones;
 use core\ConfigGlobal;
 use personas\model\entity\GestorPersonaSSSC;
 use personas\model\entity\GestorPersonaSacd;
 use personas\model\entity\PersonaSacd;
-use procesos\model\entity\GestorActividadProcesoTarea;
 use ubis\model\entity\Ubi;
 use usuarios\model\entity\Usuario;
 use web\DateTimeLocal;
@@ -140,6 +138,7 @@ switch ($Qque) {
 if (empty($inicioIso) OR empty($finIso)) {
     exit ("<br>"._("falta determinar un periodo"));
 }
+/*
 $aWhere = [];
 $aOperador = [];
 $aWhere['f_ini'] = "'$inicioIso','$finIso'";
@@ -156,6 +155,7 @@ foreach ($cActividades as $oActividad) {
 	$a_actividades[$id_activ] = $oActividad;
 }
 unset($cActividades);
+*/
 
 $s=0;
 $array_actividades = [];
@@ -178,129 +178,134 @@ foreach ($cPersonas as $oPersona) {
     $array_actividades[$id_nom]['txt']['t_nom_tipo'] = $oActividadesSacdFunciones->getTraduccion('t_nom_tipo',$idioma);
 	
 	$array_actividades[$id_nom]['nom_ap']=$nom_ap;
-    /*
-     * No parece que se use
-	$email = '';
-	$oGeTeleco = new GestorTelecoPersona();
-	$cTelecos = $oGeTeleco->getTelecos(array('id_nom'=>$id_nom,'tipo_teleco'=>'e-mail','desc_teleco'=>13));
-	if (!empty($cTelecos)) {
-		$email = $cTelecos[0]->getNum_teleco();
-	}
-	$array_actividades[$id_nom]['email']=$email;
-     */
 	
 	// busco los datos de las actividades 
-	$GesCargoOAsistente = new GestorCargoOAsistente();
-	$cCargoOAsistente = $GesCargoOAsistente->getCargoOAsistente($id_nom);
+	$aWhereAct = [];
+	$aOperadorAct = [];
+	$aWhereAct['f_ini']="'$finIso'";
+	$aOperadorAct['f_ini']='<=';
+	$aWhereAct['f_fin']="'$inicioIso'";
+	$aOperadorAct['f_fin']='>=';
+	$aWhereAct['status']='2';
+	/*
+	 $aWhere = ['id_nom' => $id_nom, 'plaza' => Asistente::PLAZA_PEDIDA];
+	 $aOperador = ['plaza' => '>='];
+	 */
+	$aWhere = ['id_nom' => $id_nom];
+	$aOperador = [];
+	
+	$oGesActividadCargo = new GestorActividadCargo();
+	$cAsistentes = $oGesActividadCargo ->getAsistenteCargoDeActividad($aWhere,$aOperador,$aWhereAct,$aOperadorAct);
+	
 	$ord_activ = [];
-	foreach ($cCargoOAsistente as $oCargoOAsistente) {
-		$id_activ=$oCargoOAsistente->getId_activ();
+	foreach ($cAsistentes as $aAsistente) {
+	    $id_activ = $aAsistente['id_activ'];
+	    $propio = $aAsistente['propio'];
+	    $plaza = $aAsistente['plaza'];
+	    $id_cargo = empty($aAsistente['id_cargo'])? '' : $aAsistente['id_cargo'];
+	    
         $_SESSION['oPermActividades']->setId_activ($id_activ);
-		if (in_array($id_activ,$a_id_actividades)) {
-			$propio = $oCargoOAsistente->getPropio();
-			$id_cargo = $oCargoOAsistente->getId_cargo();
 			
-            if(core\ConfigGlobal::is_app_installed('procesos')) {
-                $permiso_ver = FALSE;
-                $oPermActiv = $_SESSION['oPermActividades']->getPermisoActual('datos');
-                $oPermSacd = $_SESSION['oPermActividades']->getPermisoActual('sacd');
-                $oPermAsisSacd = $_SESSION['oPermActividades']->getPermisoActual('asistentesSacd');
-                // para ver la actividad:
-                if ($oPermActiv->have_perm_activ('ver') === FALSE) {
-                    continue;
+        if(core\ConfigGlobal::is_app_installed('procesos')) {
+            $permiso_ver = FALSE;
+            $oPermActiv = $_SESSION['oPermActividades']->getPermisoActual('datos');
+            $oPermSacd = $_SESSION['oPermActividades']->getPermisoActual('sacd');
+            $oPermAsisSacd = $_SESSION['oPermActividades']->getPermisoActual('asistentesSacd');
+            // para ver la actividad:
+            if ($oPermActiv->have_perm_activ('ver') === FALSE) {
+                continue;
+            }
+            // si es solo cargo, tiene propio='f' como sacd de la actividad
+            if (!empty($id_cargo)) {
+                if ($oPermSacd->have_perm_activ('ver') === TRUE) {
+                    $permiso_ver = TRUE;
                 }
-                // si es solo cargo, tiene propio='f' como sacd de la actividad
-                if (!empty($id_cargo)) {
-                    if ($oPermSacd->have_perm_activ('ver') === TRUE) {
-            			$permiso_ver = TRUE;
-                    }
-                    //si también asiste. tiene propio = 't'
-                    if ($propio == 't' && $oPermAsisSacd->have_perm_activ('ver') === TRUE) {
-            			$permiso_ver = TRUE;
-                    }
-                } else {
-                    // sólo asiste
-                    if ($oPermAsisSacd->have_perm_activ('ver') === TRUE) {
-            			$permiso_ver = TRUE;
-                    }
+                //si también asiste. tiene propio = 't'
+                if ($propio == 't' && $oPermAsisSacd->have_perm_activ('ver') === TRUE) {
+                    $permiso_ver = TRUE;
                 }
             } else {
-                $permiso_ver = TRUE;
+                // sólo asiste
+                if ($oPermAsisSacd->have_perm_activ('ver') === TRUE) {
+                    $permiso_ver = TRUE;
+                }
             }
-            
-            if ($permiso_ver === FALSE) { continue; }
-			
-			$oActividad = $a_actividades[$id_activ];
-			$id_tipo_activ = $oActividad->getId_tipo_activ();
-			$id_ubi = $oActividad->getId_ubi();
-			$lugar_esp = $oActividad->getLugar_esp();
-			$oF_ini = $oActividad->getF_ini();
-			$oF_fin = $oActividad->getF_fin();
-			$h_ini = $oActividad->getH_ini();
-			$h_fin = $oActividad->getH_fin();
-			$observ = $oActividad->getObserv();
-			
-			$f_ini=$oF_ini->formatRoman();
-			$f_fin=$oF_fin->formatRoman();
+        } else {
+            $permiso_ver = TRUE;
+        }
+        
+        if ($permiso_ver === FALSE) { continue; }
+        
+        $oActividad = $a_actividades[$id_activ];
+        $id_tipo_activ = $oActividad->getId_tipo_activ();
+        $id_ubi = $oActividad->getId_ubi();
+        $lugar_esp = $oActividad->getLugar_esp();
+        $oF_ini = $oActividad->getF_ini();
+        $oF_fin = $oActividad->getF_fin();
+        $h_ini = $oActividad->getH_ini();
+        $h_fin = $oActividad->getH_fin();
+        $observ = $oActividad->getObserv();
+        
+        $f_ini=$oF_ini->formatRoman();
+        $f_fin=$oF_fin->formatRoman();
 
-			if (!empty($h_ini)) {
-			   	$h_ini = preg_replace('/(\d{2}):(\d{2}):(\d{2})/','\1:\2',$h_ini);
-			   	$f_ini.=" ($h_ini)";
-			}
-			if (!empty($h_fin)) {
-			   	$h_fin = preg_replace('/(\d{2}):(\d{2}):(\d{2})/','\1:\2',$h_fin);
-			   	$f_fin.=" ($h_fin)";
-			}
+        if (!empty($h_ini)) {
+               $h_ini = preg_replace('/(\d{2}):(\d{2}):(\d{2})/','\1:\2',$h_ini);
+               $f_ini.=" ($h_ini)";
+        }
+        if (!empty($h_fin)) {
+               $h_fin = preg_replace('/(\d{2}):(\d{2}):(\d{2})/','\1:\2',$h_fin);
+               $f_fin.=" ($h_fin)";
+        }
 
-			$oTipoActiv= new TiposActividades($id_tipo_activ);
-			$ssfsv=$oTipoActiv->getSfsvText();
-			$sasistentes=$oTipoActiv->getAsistentesText();
-			$sactividad=$oTipoActiv->getActividadText();
-			$snom_tipo=$oTipoActiv->getNom_tipoText();
-			// lugar 
-			if (empty($lugar_esp)) {
-			    $oCasa = Ubi::NewUbi($id_ubi);
-				$nombre_ubi = $oCasa->getNombre_ubi();
-			} else {
-				$nombre_ubi = $lugar_esp;
-			}
+        $oTipoActiv= new TiposActividades($id_tipo_activ);
+        $ssfsv=$oTipoActiv->getSfsvText();
+        $sasistentes=$oTipoActiv->getAsistentesText();
+        $sactividad=$oTipoActiv->getActividadText();
+        $snom_tipo=$oTipoActiv->getNom_tipoText();
+        // lugar 
+        if (empty($lugar_esp)) {
+            $oCasa = Ubi::NewUbi($id_ubi);
+            $nombre_ubi = $oCasa->getNombre_ubi();
+        } else {
+            $nombre_ubi = $lugar_esp;
+        }
 
-			// ctr que organiza:
-			$GesCentroEncargado = new GestorCentroEncargado();
-			$ctrs = '';
-			foreach($GesCentroEncargado->getCentrosEncargadosActividad($id_activ) as $oCentro) {;
-				if (!empty($ctrs)) $ctrs.=", ";
-				$ctrs .= $oCentro->getNombre_ubi();
-			}
+        // ctr que organiza:
+        $GesCentroEncargado = new GestorCentroEncargado();
+        $ctrs = '';
+        foreach($GesCentroEncargado->getCentrosEncargadosActividad($id_activ) as $oCentro) {;
+            if (!empty($ctrs)) $ctrs.=", ";
+            $ctrs .= $oCentro->getNombre_ubi();
+        }
 
-			$cargo='';
-			if (!empty($id_cargo) && !array_key_exists($id_cargo, $aIdCargos_sacd)) {
-				$cargo='te carrec';
-			}
-			$array_act=array( "propio" => $propio,
-								"f_ini" => $f_ini,
-								"f_fin" =>		$f_fin, 
-								"nombre_ubi" =>	$nombre_ubi, 
-								"id_activ" 	=>	$id_activ, 
-								"sfsv" 		=>	$ssfsv, 
-								"asistentes" =>	$sasistentes, 
-								"actividad" =>	$sactividad,
-								"nom_tipo" =>	$snom_tipo, 
-								"observ" =>		$observ, 
-								"cargo" =>		$cargo, 
-								"encargado" =>	$ctrs
-							);
-			//if (!empty($id_activ)) { $array_actividades[$id_nom]['actividades'][]= $array_act; }
-			// para ordenar por fecha_ini
-			$f_ord = $oF_ini->format('Ymd');
-			// ojo. Si hay más de una actividad que empieza el mismo día, hay que poner algo para distinguirlas: les sumo un dia.
-			if (isset($ord_activ) && array_key_exists($f_ord,$ord_activ)) {
-				$f_ord++;
-				$ord_activ[$f_ord]=$array_act;
-			} else {
-				$ord_activ[$f_ord]=$array_act;
-			}
-		}
+        $cargo='';
+        if (!empty($id_cargo) && !array_key_exists($id_cargo, $aIdCargos_sacd)) {
+            $cargo='te carrec';
+        }
+        $array_act=array( "propio" => $propio,
+                            "f_ini" => $f_ini,
+                            "f_fin" =>		$f_fin, 
+                            "nombre_ubi" =>	$nombre_ubi, 
+                            "id_activ" 	=>	$id_activ, 
+                            "sfsv" 		=>	$ssfsv, 
+                            "asistentes" =>	$sasistentes, 
+                            "actividad" =>	$sactividad,
+                            "nom_tipo" =>	$snom_tipo, 
+                            "observ" =>		$observ, 
+                            "cargo" =>		$cargo, 
+                            "encargado" =>	$ctrs
+                        );
+        //if (!empty($id_activ)) { $array_actividades[$id_nom]['actividades'][]= $array_act; }
+        // para ordenar por fecha_ini
+        $f_ord = $oF_ini->format('Ymd');
+        // ojo. Si hay más de una actividad que empieza el mismo día, hay que poner algo para distinguirlas: les sumo un dia.
+        if (isset($ord_activ) && array_key_exists($f_ord,$ord_activ)) {
+            $f_ord++;
+            $ord_activ[$f_ord]=$array_act;
+        } else {
+            $ord_activ[$f_ord]=$array_act;
+        }
 	}
 	if (!empty($ord_activ)) {
 		ksort($ord_activ);
