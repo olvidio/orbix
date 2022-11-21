ff2d6386e39c04bc87bdd545d5ddf324f46205c6
dani <dani@dani-ThinkPad-T540p>
2022-11-21 17:44:04 +0100

    redondeo duracion acttividades pasarela

--- apps/web/DateTimeLocal.php
+++ apps/web/DateTimeLocal.php
@@ -164,7 +164,7 @@ class DateTimeLocal extends DateTime
      * @param DateTimeZone|NULL $timezone
      * @return DateTime|false
      */
-    public static function createFromFormat(string $format='', string $datetime='', DateTimeZone $timezone = NULL)
+    public static function createFromFormat($format='', $datetime='', $timezone = NULL)
     {
         $extnd_dt = new static();
         $parent_dt = parent::createFromFormat($format, $datetime, $timezone);
@@ -180,7 +180,7 @@ class DateTimeLocal extends DateTime
      * @param string $format
      * @return string|array
      */
-    public function format(string $format='')
+    public function format($format='')
     {
         $english = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
         $local = array(_("lunes"), _("martes"), _("miércoles"), _("jueves"), _("viernes"), _("sábado"), _("domingo"));
