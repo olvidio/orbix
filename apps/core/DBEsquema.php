3c3a3d2e0e3d7aadbd5d9af1f2351698f15f3af2
dani <dani@dani-ThinkPad-T540p>
2023-07-10 13:25:41 +0200

    revisar para crear nuevo esquema

--- apps/core/DBEsquema.php
+++ apps/core/DBEsquema.php
@@ -348,15 +348,22 @@ class DBEsquema
         //          -U postgres -h 127.0.0.1 pruebas-comun" > /var/www/pruebas/log/db/Acse-crAcse.comun.sql
 
         $dbname = $this->getDbName();
-        $host_local = $this->getHost();
+        // para conexiones locales via sockets
+        $host_local = '/var/run/postgresql';
         // leer esquema
         //$command_ssh = "/usr/bin/ssh aquinate@192.168.200.16";
         $command_ssh = "/usr/bin/ssh " . $this->getSsh_user() . "@" . $this->getHost();
         $command_db = "/usr/bin/pg_dump -s --schema=\\\\\\\"" . $this->getRef() . "\\\\\\\" ";
         $command_db .= "-U postgres -h $host_local $dbname";
         $command = "$command_ssh \"$command_db\" > " . $this->getFileRef();
-        //echo "$command<br>";
+        $command .= " > " . $this->getFileLog() . " 2>&1";
         passthru($command); // no output to capture so no need to store it
+        $error = file_get_contents($this->getFileLog());
+        if (trim($error) != '') {
+            if (ConfigGlobal::is_debug_mode()) {
+                echo sprintf(_("PSQL ERROR IN COMMAND(0): %s<br> mirar: %s<br>"), $command, $this->getFileLog());
+            }
+        }
     }
 
     private function leer_local()
