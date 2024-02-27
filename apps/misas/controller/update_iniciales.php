f78b8ece5f8339a93ce19b0cb0e51673654225f2
dani <dani@dani-ThinkPad-T540p>
2024-02-27 11:55:20 +0100

    update iniciales

--- apps/misas/controller/update_iniciales.php
+++ apps/misas/controller/update_iniciales.php
@@ -2,18 +2,8 @@
 
 // INICIO Cabecera global de URL de controlador *********************************
 
-use encargossacd\model\entity\Encargo;
-use encargossacd\model\entity\EncargoTipo;
-use encargossacd\model\entity\GestorEncargoHorario;
-use misas\domain\EncargoDiaId;
-use misas\domain\EncargoDiaTend;
-use misas\domain\EncargoDiaTstart;
-use misas\domain\entity\EncargoDia;
-use misas\domain\repositories\EncargoDiaRepository;
-use web\DateTimeLocal;
-use ubis\model\entity\Ubi;
-use misas\domain\repositories\InicialesSacdRepository;
 use misas\domain\entity\InicialesSacd;
+use misas\domain\repositories\InicialesSacdRepository;
 
 require_once("apps/core/global_header.inc");
 // Archivos requeridos por esta url **********************************************
@@ -22,25 +12,31 @@ require_once("apps/core/global_header.inc");
 require_once("apps/core/global_object.inc");
 // FIN de  Cabecera global de URL de controlador ********************************
 
-$Qid_sacd = (string)filter_input(INPUT_POST, 'id_sacd');
+$Qid_sacd = (int)filter_input(INPUT_POST, 'id_sacd');
 $Qiniciales = (string)filter_input(INPUT_POST, 'iniciales');
 $Qcolor = (string)filter_input(INPUT_POST, 'color');
 
-echo 'echo: '.$Qid_sacd, $Qiniciales, $Qcolor.'<br>';
-
 $error_txt = '';
 
-$InicialesSacd = new InicialesSacd();
-//$InicialesSacd = $InicialesSacdRepository->findById($Qid_sacd);
-$inicialesSacd = $InicialesSacd->setId_nom($Qid_sacd);
-$inicialesSacd = $InicialesSacd->setIniciales('MG');
-//$inicialesSacd = $InicialesSacd->setIniciales($Qiniciales);
-$color = $InicialesSacd->setColor($Qcolor);
+$InicialesSacdRepository = new InicialesSacdRepository();
+$InicialesSacd = $InicialesSacdRepository->findById($Qid_sacd);
+if (is_null($InicialesSacd)) {
+    $InicialesSacd = new InicialesSacd();
+    $InicialesSacd->setId_nom($Qid_sacd);
+}
+$InicialesSacd->setIniciales($Qiniciales);
+$InicialesSacd->setColor($Qcolor);
 
-//if ($InicialesSacd->DBGuardar($InicialesSacd) === FALSE) {
-if ($InicialesSacd->Guardar($InicialesSacd) === FALSE) {
-        echo 'ERROR'.$InicialesSacd->getErrorTxt();
-    $error_txt .= $InicialesSacd->getErrorTxt();
+if ($InicialesSacdRepository->Guardar($InicialesSacd) === FALSE) {
+    $error_txt .= $InicialesSacdRepository->getErrorTxt();
+}
+
+if (empty($error_txt)) {
+    $jsondata['success'] = true;
+    $jsondata['mensaje'] = 'Tot correcte.';
+} else {
+    $jsondata['success'] = false;
+    $jsondata['mensaje'] = $error_txt;
 }
 
 //Aunque el content-type no sea un problema en la mayoría de casos, es recomendable especificarlo
