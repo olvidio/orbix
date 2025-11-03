032e77f1169d788889d64110567d3e8c0abed9f4
dani <dani@dani-ThinkPad-T540p>
2025-11-03 13:28:35 +0100

    planning para personaSacd

--- apps/planning/controller/planning_persona_que.php
+++ apps/planning/controller/planning_persona_que.php
@@ -48,7 +48,7 @@ $Qobj_pau = (string)filter_input(INPUT_POST, 'obj_pau');
 $Qna = (string)filter_input(INPUT_POST, 'na');
 
 $mes = date('m');
-$fin_m = $_SESSION['oConfig']->getMesFinStgr();
+//$fin_m = $_SESSION['oConfig']->getMesFinStgr();
 if ($mes > $fin_m) {
     $periodo_txt = sprintf(_("(por defecto: periodo desde 1/%s hasta 31/5)"), $fin_m + 1);
 } else {
@@ -116,6 +116,9 @@ switch ($Qobj_pau) {
         break;
     case 'PersonaEx':
         $personas_txt = _("de paso");
+        break;
+    case 'PersonaSacd':
+        $personas_txt = _("sacd");
         break;
     default:
         $err_switch = sprintf(_("opción no definida en switch en %s, linea %s"), __FILE__, __LINE__);
