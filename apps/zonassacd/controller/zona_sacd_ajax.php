d609011deac974b2826dadafb66b9c33896e887f
dani <dani@dani-ThinkPad-T540p>
2026-04-15 13:38:18 +0200

    zonassacd delete legacy

--- apps/zonassacd/controller/zona_sacd_ajax.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Compatibilidad: URL historica bajo apps/. Preferir
- * frontend/zonassacd/controller/zona_sacd_ajax.php para menus y enlaces nuevos.
- */
-require __DIR__ . '/../../../frontend/zonassacd/controller/zona_sacd_ajax.php';
