d6a4941e84b30805969b5229c8e74a6c4c879943
dani <dani@dani-ThinkPad-T540p>
2026-05-22 13:02:10 +0200

    migrations 6

--- db/migrations/202604070000_teleco_personas_casos_especiales__sv.sql
+++
@@ -1,6 +0,0 @@
--- Casos especiales: vistas materializadas que bloquean el tipo de id_tipo_teleco.
-DROP MATERIALIZED VIEW "H-Hv".d_teleco_personas_dl;
-DROP MATERIALIZED VIEW "M-Mv".d_teleco_personas_dl;
-
-ALTER TABLE publicv.d_teleco_personas ALTER COLUMN id_tipo_teleco TYPE int USING id_tipo_teleco::integer;
-ALTER TABLE publicv.d_teleco_personas ALTER COLUMN id_tipo_teleco SET NOT NULL;
