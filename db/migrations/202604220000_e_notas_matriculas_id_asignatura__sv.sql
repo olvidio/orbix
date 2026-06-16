d0afd11f582525ab8886e359003011918a1bb4c0
dani <dani@dani-ThinkPad-T540p>
2026-06-16 12:06:30 +0200

    cambio id_asignatura en actas

--- db/migrations/202604220000_e_notas_matriculas_id_asignatura__sv.sql
+++ db/migrations/202604220000_e_notas_matriculas_id_asignatura__sv.sql
@@ -21,4 +21,15 @@ UPDATE publicv.d_matriculas_activ SET id_asignatura = 3408 WHERE id_asignatura =
 UPDATE publicv.d_matriculas_activ SET id_asignatura = 3409 WHERE id_asignatura = 3500;
 UPDATE publicv.d_matriculas_activ SET id_asignatura = 3410 WHERE id_asignatura = 3555;
 
+UPDATE publicv.e_actas SET id_asignatura = 3401 WHERE id_asignatura = 4444;
+UPDATE publicv.e_actas SET id_asignatura = 3402 WHERE id_asignatura = 4445;
+UPDATE publicv.e_actas SET id_asignatura = 3403 WHERE id_asignatura = 4455;
+UPDATE publicv.e_actas SET id_asignatura = 3404 WHERE id_asignatura = 4456;
+UPDATE publicv.e_actas SET id_asignatura = 3405 WHERE id_asignatura = 4457;
+UPDATE publicv.e_actas SET id_asignatura = 3406 WHERE id_asignatura = 4458;
+UPDATE publicv.e_actas SET id_asignatura = 3407 WHERE id_asignatura = 4459;
+UPDATE publicv.e_actas SET id_asignatura = 3408 WHERE id_asignatura = 4460;
+UPDATE publicv.e_actas SET id_asignatura = 3409 WHERE id_asignatura = 3500;
+UPDATE publicv.e_actas SET id_asignatura = 3410 WHERE id_asignatura = 3555;
+
 DELETE FROM publicv.d_matriculas_activ WHERE id_asignatura = 0;
