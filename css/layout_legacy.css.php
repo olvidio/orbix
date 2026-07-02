06dcc39b9daa1f4532be12e094f31bcf7cb5eacc
dani <dani@dani-ThinkPad-T540p>
2026-07-02 10:54:08 +0200

    css

--- css/layout_legacy.css.php
+++ css/layout_legacy.css.php
@@ -1,6 +1,6 @@
 <style>
 :root {
-    --legacy-chrome-offset: 72px;
+    --legacy-chrome-offset: 75px;
     --legacy-chrome-z: 100010;
 }
 
@@ -10,8 +10,15 @@
     left: 0;
     right: 0;
     z-index: var(--legacy-chrome-z);
+    box-sizing: border-box;
+    padding-top: 3px;
     background: <?= $fondo_claro ?>;
     box-shadow: 0 2px 8px rgba(15, 23, 42, 0.08);
+}
+
+/* menu_horizontal/menu_vertical usan margin:-10px; con shell fijo queda pegado al borde superior. */
+body.layout-legacy .orbix-layout-legacy #menu {
+    margin: 0;
 }
 
 body.layout-legacy #contenido_sin_menus {
---
+++ db/migrations/202606120002_aux_menus_bug__sv-e.sql
@@ -0,0 +1,3 @@
+-- aux_menus:  (sv-e, datos, todos los esquemas *v).
+
+DELETE FROM *.aux_menus WHERE menu = 'Informar de un bug';
